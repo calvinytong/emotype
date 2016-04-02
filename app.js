e1ec175859bea87b3fbfa235b378284bd7178126
calvinytong <calvinytong@gmail.com>
2016-04-02 00:39:54 -0400

    old fasion style

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 var express = require('express');
-var AlchemyAPI = require('alchemy-api');
-var alchemy = new AlchemyAPI('73fdf24054081a04c8778d53196c022aac5195b8');
 var app = express();
 var port = process.env.PORT || 3000;
+var request = require('request');
 
 var myText = "Whoa, AlchemyAPI's Node.js SDK is really great, I can't wait to build my app!";
+var alchemyurl = 'http://gateway-a.watsonplatform.net/'
+
 
 app.get('/', function(req, res) {
   res.send('Hello World');
-  alchemy.sentiment("text", myText, {}, function(response) {
-    res.send("Sentiment: " + response["docSentiment"]["type"]);
+  var propertiesObject = { apikey:'test1', text:'textbody', outputMode: 'json' };
+  request({url:alchemyurl + 'calls/text/TextGetEmotion', qs = propertiesObject, function(err, response, body {
+      console.log("Get response: " + response.statusCode);
+    });
   });
 });
 
