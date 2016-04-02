8dde35b0f15137446d19eb811580d6e66c9ea084
calvinytong <calvinytong@gmail.com>
2016-04-02 00:14:27 -0400

    asdf

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var AlchemyAPI = require('node-alchemy');
+var AlchemyAPI = require('alchemy-api');
 var alchemy = new AlchemyAPI('73fdf24054081a04c8778d53196c022aac5195b8');
 var app = express();
 var port = process.env.PORT || 3000;
@@ -8,7 +8,7 @@ var myText = "Whoa, AlchemyAPI's Node.js SDK is really great, I can't wait to bu
 
 app.get('/', function(req, res) {
   res.send('Hello World');
-  alchemyapi.sentiment("text", myText, {}, function(response) {
+  alchemy.sentiment("text", myText, {}, function(response) {
     res.send("Sentiment: " + response["docSentiment"]["type"]);
   });
 });
