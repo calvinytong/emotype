2f409566223b77b64559999a8332d3eaf45c0d43
calvinytong <calvinytong@gmail.com>
2016-04-02 01:16:28 -0400

    asf

--- app.js
+++ app.js
@@ -4,22 +4,22 @@ var port = process.env.PORT || 3000;
 var request = require('request');
 
 
-var myText = "Whoa, AlchemyAPI's Node.js SDK is really great, I can't wait to build my app!";
+// var myText = "Whoa, AlchemyAPI's Node.js SDK is really great, I can't wait to build my app!";
 var alchemyurl = 'http://gateway-a.watsonplatform.net/'
 var key = '73fdf24054081a04c8778d53196c022aac5195b8'
 
 app.get('/', function(req, res) {
-  res.send("hello world");
+  text = req.query.text;
   var params = {
         "apikey": key,
-        "text": myText,
+        "text": text,
         "outputMode": "json",
         "showSourceText": 1
       };
   request({url:alchemyurl + 'calls/text/TextGetEmotion', qs:params}, function(err, response, body) {
     var data = JSON.parse(body);
     console.log(data.docEmotions);
-    console.log("Get response: ");
+    res.send(data);
   });
 });
 app.listen(port, function() {
