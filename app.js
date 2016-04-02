d4ca0c6aa14f9fd4143c3235807baaf99fffde41
calvinytong <calvinytong@gmail.com>
2016-04-02 00:08:24 -0400

    added alchemy

--- app.js
+++ app.js
@@ -4,8 +4,16 @@ var alchemy = new AlchemyAPI('73fdf24054081a04c8778d53196c022aac5195b8');
 var app = express();
 var port = process.env.PORT || 3000;
 
-app.get('/', function(req, res) { res.status(200).send('Hello World')});
+var myText = "Whoa, AlchemyAPI's Node.js SDK is really great, I can't wait to build my app!";
+
+app.get('/', function(req, res) {
+  res.send('Hello World');
+  alchemyapi.sentiment("text", myText, {}, function(response) {
+    res.send("Sentiment: " + response["docSentiment"]["type"]);
+  });
+});
 
 app.listen(port, function() {
+
     console.log('Listening on port ' + port);
 });
